72085f53ff12e42f811834bb7196bfd278449ad8
Ramora0 <99ilovecats99@gmail.com>
2023-12-25 16:49:28 -0500

    woops

--- src/main/java/com/kingdoms/Kingdoms.java
+++ src/main/java/com/kingdoms/Kingdoms.java
@@ -2,8 +2,6 @@ package com.kingdoms;
 
 import com.kingdoms.events.EventBus;
 import com.kingdoms.ui.UI;
-import com.kingdoms.ui.scenes.MainScene;
-import com.kingdoms.world.World;
 
 import processing.core.PApplet;
 import processing.event.MouseEvent;
@@ -21,8 +19,8 @@ public class Kingdoms extends PApplet {
     EventBus.subscribeStaticMethods();
     UI.init();
 
-    World.generateWorld(20);
-    UI.changeScene(new MainScene());
+    // World.generateWorld(20);
+    // UI.changeScene(new MainScene());
   }
 
   public void draw() {
