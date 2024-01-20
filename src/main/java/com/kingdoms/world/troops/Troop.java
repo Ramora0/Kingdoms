64ff728bed364d0c9e3983d29fe037c61453ad03
Ramora0 <99ilovecats99@gmail.com>
2024-01-19 20:16:52 -0500

    Tile moving null fix

--- src/main/java/com/kingdoms/world/troops/Troop.java
+++ src/main/java/com/kingdoms/world/troops/Troop.java
@@ -93,8 +93,8 @@ public abstract class Troop extends Updateable implements JSONSerializable, JSON
     }
 
     tile = path.get(0);
-    path.remove(0);
-    Tile.move(this, tile, path.get(0));
+    Tile moveTo = path.remove(0);
+    Tile.move(this, tile, moveTo);
 
     Troop.combat(tile.getTroops(player), tile.getTroops(World.other(player))); // TODO: Make sure this combat only
                                                                                // happens once! Shouldnt be run by both
